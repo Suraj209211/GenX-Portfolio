b34b5f9569ee66e6e1421f47047490920007c23a
SURAJ MAHAPATRA <85944171+Suraj209211@users.noreply.github.com>
2023-11-08 16:21:09 +0000

    Pending changes exported from your codespace

--- pages/index.js
+++ pages/index.js
@@ -29,19 +29,21 @@ const Home = () => {
 </div>
 {/* Images */}
 <div className='w-[1200px] h-full absolute right-0 bottom-0'>
-  {/* bg-image */}
+{/* bg-image */}
   <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'></div>
 </div>
+
 {/* {Particle} */}
-<div>Particles</div>
+<ParticlesContainer />
 {/* Avatar */}
+ 
 <motion.div 
 variants={fadeIn('up',0.2)} 
 initial='hidden' 
 animate='show'
 transition={{duration: 1, ease: 'easeInOut '}}
 className='w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[2%]'>
-  <Avatar />
+<Avatar />
 </motion.div>
 </div> 
   );
